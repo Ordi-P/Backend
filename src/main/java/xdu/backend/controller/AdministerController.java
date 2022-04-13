a6139ab824e7e7cdb714df7fefa08c2c20f40cc6
Ordi_P <63042018+Ordi-P@users.noreply.github.com>
2022-04-13 21:41:47 +0800

    searchbook和comment完成

--- src/main/java/xdu/backend/controller/AdministerController.java
+++ src/main/java/xdu/backend/controller/AdministerController.java
@@ -1,13 +1,20 @@
 package xdu.backend.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import xdu.backend.Dao.BookMapper;
 import xdu.backend.Dao.BorrowMapper;
+import xdu.backend.Dao.CommentMapper;
 import xdu.backend.Dao.ReserveMapper;
 import xdu.backend.pojo.Book;
+import xdu.backend.pojo.BookExample;
+import xdu.backend.pojo.Comment;
 import xdu.backend.pojo.User;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/administer")
@@ -18,6 +25,8 @@ public class AdministerController {
     ReserveMapper reserveMapper;
     @Autowired
     BookMapper bookMapper;
+    @Autowired
+    CommentMapper commentMapper;
 
     @GetMapping(value = "/addbook")
     @ResponseBody
@@ -65,6 +74,49 @@ public class AdministerController {
             book.setNname(bookname);
         book.setFree(booknum);
         int res = bookMapper.updateByPrimaryKeySelective(book);
+        if(res > 0){
+            return "success";
+        }
+        return "failed";
+    }
+
+    @GetMapping(value = "/searchbook")
+    @ResponseBody
+    JSONObject searchbook(@RequestParam(value="bookid",required=true) String bookid,
+                   @RequestParam(value="bookname",required=true) String bookname){
+        if(bookid == null && bookname == null){
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("result", "failed");
+            return jsonObject;
+        }
+
+        BookExample example1 = new BookExample();
+        example1.createCriteria().andBook_idEqualTo(bookid);
+        List<Book> res = bookMapper.selectByExample(example1);
+
+        JSONObject ret = (JSONObject) JSON.toJSON(res);
+
+        ret.put("result", "success");
+
+        if(!res.isEmpty()){
+            return ret;
+        }
+        return ret;
+    }
+
+    @GetMapping(value = "/comment")
+    @ResponseBody
+    String comment(@RequestParam(value="bookid",required=true) String bookid,
+                   @RequestParam(value="comment",required=true) String comment){
+        if(bookid == null || comment == null){
+            return "failed";
+        }
+
+        Comment comment1 = new Comment();
+        comment1.setBook_id(bookid);
+        comment1.setCcomment(comment);
+
+        int res = commentMapper.insert(comment1);
         if(res > 0){
             return "success";
         }

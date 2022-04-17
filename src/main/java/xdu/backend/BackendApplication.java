e5c253cdf4163413f088604ceb10165b241bd117
MAPS-Shen <60740186+MAPS-Shen@users.noreply.github.com>
2022-04-17 15:52:46 +0800

    添加过滤器尝试解决跨域问题

--- src/main/java/xdu/backend/BackendApplication.java
+++ src/main/java/xdu/backend/BackendApplication.java
@@ -3,9 +3,11 @@ package xdu.backend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan("xdu.backend.Dao")
+@ServletComponentScan("xdu.backend.config")
 public class BackendApplication {
 
     public static void main(String[] args) {

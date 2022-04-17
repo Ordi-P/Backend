f175fc4ac3951b843494e8d0537499a4c598eaed
MAPS-Shen <60740186+MAPS-Shen@users.noreply.github.com>
2022-04-17 14:52:12 +0800

    更新登录,注销,注册,获取用户登录,信息更新数据库地址

--- src/main/java/xdu/backend/config/WebConfig.java
+++ src/main/java/xdu/backend/config/WebConfig.java
@@ -1,6 +1,7 @@
 package xdu.backend.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.ArrayList;
 import java.util.List;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -18,9 +20,8 @@ public class WebConfig implements WebMvcConfigurer {
         InterceptorRegistration ir = registry.addInterceptor(loginInterceptor);
         ir.addPathPatterns("/*");
         List<String> irs = new ArrayList<>();
-        irs.add("/api/*");
-        irs.add("/wechat/*");
-        irs.add("/oauth");
+        irs.add("/login");
+        irs.add("/register");
         ir.excludePathPatterns(irs);
     }
 }
--- src/main/java/xdu/backend/pojo/User.java
+++ src/main/java/xdu/backend/pojo/User.java
@@ -5,33 +5,25 @@ import java.io.Serializable;
 
 public class User implements Serializable {
 
-    private String userId;
+    private String id;
     private String userName;
-    private String nickName;
-    private Long phoneNumber;
     private String password;
-    private Integer privilege;
-    private String address;
 
     public User() {
     }
 
-    public User(String userId, String userName, String nickName, Long phoneNumber, String password, Integer privilege, String address) {
-        this.userId = userId;
+    public User(String id, String userName, String password) {
+        this.id = id;
         this.userName = userName;
-        this.nickName = nickName;
-        this.phoneNumber = phoneNumber;
         this.password = password;
-        this.privilege = privilege;
-        this.address = address;
     }
 
-    public String getUserId() {
-        return userId;
+    public String getId() {
+        return id;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getUserName() {
@@ -42,22 +34,6 @@ public class User implements Serializable {
         this.userName = userName;
     }
 
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
     public String getPassword() {
         return password;
     }
@@ -66,32 +42,12 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    public Integer getPrivilege() {
-        return privilege;
-    }
-
-    public void setPrivilege(Integer privilege) {
-        this.privilege = privilege;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     @Override
     public String toString() {
         return "User{" +
-                "userId='" + userId + '\'' +
+                "id='" + id + '\'' +
                 ", userName='" + userName + '\'' +
-                ", nickName='" + nickName + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
                 ", password='" + password + '\'' +
-                ", privilege=" + privilege +
-                ", address='" + address + '\'' +
                 '}';
     }
 }

433930d2ea36288e59aa065cc7d2e556f3482252
Ordi_P <63042018+Ordi-P@users.noreply.github.com>
2022-04-22 14:12:52 +0800

    对book增删的接口做了修改，增加了对BookMeta的操作

--- src/main/java/xdu/backend/config/LoginInterceptor.java
+++ src/main/java/xdu/backend/config/LoginInterceptor.java
@@ -1,64 +1,64 @@
-//package xdu.backend.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//import org.springframework.util.StringUtils;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//import org.springframework.web.servlet.ModelAndView;
-//import xdu.backend.Dao.UserDao;
-//
-//import javax.servlet.http.Cookie;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//
-//@Component
-//public class LoginInterceptor implements HandlerInterceptor {
-//
-//    @Autowired
-//    UserDao userDao;
-//
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        Cookie[] cookies = request.getCookies();
-//
-//        response.setHeader("Access-Control-Allow-Credentials", "true");
-//        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//        response.setHeader("Access-Control-Allow-Headers", "true");
-//        response.setHeader("Set-Cookie", "token=cowshield");
-//
-//        if (null == cookies) {
-//            response.getWriter().write("failed");
-//            return false;
-//        }
-//        String cookieValue = null;
-//        for (Cookie item : cookies) {
-//            if ("cookieUserName".equals(item.getName())) {
-//                cookieValue = item.getValue();
-//                break;
-//            }
-//        }
-//        if (StringUtils.isEmpty(cookieValue)) {
-//            response.getWriter().write("failed");
-//            return false;
-//        }
-//        HttpSession session = request.getSession();
-//        Object obj = session.getAttribute("userTicket:" + cookieValue);
-//        if (null == obj) {
-//            response.getWriter().write("failed");
-//        }
-//        // 已经登录
-//        return true;
-//    }
-//
-//    @Override
-//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-//        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-//    }
-//
-//    @Override
-//    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-//        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
-//    }
-//}
-//
+package xdu.backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import xdu.backend.Dao.UserDao;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    UserDao userDao;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Cookie[] cookies = request.getCookies();
+
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Headers", "true");
+        response.setHeader("Set-Cookie", "token=cowshield");
+
+        if (null == cookies) {
+            response.getWriter().write("failed");
+            return false;
+        }
+        String cookieValue = null;
+        for (Cookie item : cookies) {
+            if ("cookieUserName".equals(item.getName())) {
+                cookieValue = item.getValue();
+                break;
+            }
+        }
+        if (StringUtils.isEmpty(cookieValue)) {
+            response.getWriter().write("failed");
+            return false;
+        }
+        HttpSession session = request.getSession();
+        Object obj = session.getAttribute("userTicket:" + cookieValue);
+        if (null == obj) {
+            response.getWriter().write("failed");
+        }
+        // 已经登录
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
+
--- src/main/java/xdu/backend/config/WebConfig.java
+++ src/main/java/xdu/backend/config/WebConfig.java
@@ -1,45 +1,45 @@
-//package xdu.backend.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Autowired
-//    LoginInterceptor loginInterceptor;
-//
-//    @Autowired
-//    AdminLoginInterceptor adminLoginInterceptor;
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        List<String> irs = new ArrayList<>();
-//        irs.add("/login");
-//        irs.add("/register");
-//        irs.add("/admin/**");
-//        InterceptorRegistration interceptor = registry.addInterceptor(adminLoginInterceptor);
-//        interceptor.addPathPatterns("/admin/**");
-//        interceptor.excludePathPatterns("/admin/login");
-//        InterceptorRegistration ir = registry.addInterceptor(loginInterceptor);
-//        ir.addPathPatterns("/**");
-//        ir.excludePathPatterns(irs);
-//    }
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedHeaders("*")
-//                .allowedOriginPatterns("*")
-//                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
-//                .maxAge(3600)
-//                .allowCredentials(true);
-//    }
-//}
+package xdu.backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    LoginInterceptor loginInterceptor;
+
+    @Autowired
+    AdminLoginInterceptor adminLoginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        List<String> irs = new ArrayList<>();
+        irs.add("/login");
+        irs.add("/register");
+        irs.add("/admin/**");
+        InterceptorRegistration interceptor = registry.addInterceptor(adminLoginInterceptor);
+        interceptor.addPathPatterns("/admin/**");
+        interceptor.excludePathPatterns("/admin/login");
+        InterceptorRegistration ir = registry.addInterceptor(loginInterceptor);
+        ir.addPathPatterns("/**");
+        ir.excludePathPatterns(irs);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}

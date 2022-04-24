d7fc19ce72b72184d5350802565dc45c12f52908
xduTd <973958891@qq.com>
2022-04-25 01:44:44 +0800

    将isbn_code参数设为非必须，增加了通过isbn_number计算isbn_code的代码； 更改了随机生成location的算法，使其更随机

--- src/main/java/xdu/backend/controller/BookController.java
+++ src/main/java/xdu/backend/controller/BookController.java
@@ -1,6 +1,7 @@
 package xdu.backend.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,22 +28,27 @@ public class BookController {
     @GetMapping(value = "/addbook")
     public JSONObject addBook(@RequestParam(value = "book_name", required = true) String book_name,
                    @RequestParam(value = "book_author", required = true) String book_author,
-                   @RequestParam(value = "isbn_code", required = true) String isbn_code,
+                   @RequestParam(value = "isbn_code", required = false) String isbn_code,
                    @RequestParam(value = "isbn_number", required = true) String isbn_number,
                    @RequestParam(value = "num", required = true) int num
-                   ){
+                   ) {
         JSONObject json = new JSONObject();
         ArrayList<Long> book_id_list = new ArrayList<>();
 
+        if (isbn_code == null) { // 如果isbn_code为空，就尝试着自己生成
+            isbn_code = generateISBNCode(isbn_number);
+        }
+
         try{
-            for (int i = 0; i < num; i++) {
+            for (int i = 0; i < num; i++) { // 添加num本书
 
                 Date date = new Date();
                 long tem = date.getTime() - 4 * 60 * 60 * 1000 - 1;
                 date.setTime(tem);
                 Timestamp reserve_time = new Timestamp(date.getTime());
 
-                Book book = new Book(book_name, book_author, "A-"+isbn_number.substring(5, 7), isbn_code, isbn_number, true, reserve_time, "13300000001");
+                // 因为isbn_number的5、6位是出版社号，所有书几乎一样，就改成10、11位，增加随机性
+                Book book = new Book(book_name, book_author, "A-" + isbn_number.substring(10, 12), isbn_code, isbn_number, true, reserve_time, "13300000001");
 
                 bookDao.addBook(book);
                 book_id_list.add(book.getBookID());
@@ -93,6 +99,74 @@ public class BookController {
         }
 
         return json;
+    }
+
+    /**
+     * 在前端未将isbn_code作为参数传递到后端时，此方
+     * 法可根据isbn_number生成isbn_code，目前匹配
+     * 的isbn_code格式有两种，XXX-X-XXX-XXXXX-X
+     * 和 XXX-X-XX-XXXXXX-X
+     *
+     * @param isbn_number
+     * @return 生成的isbn
+     */
+    private String generateISBNCode(String isbn_number) {
+        StringBuffer sb = new StringBuffer();
+        char[] ch = isbn_number.toCharArray();
+
+        // 前面6位基本不变，都是 '978-X-'
+        for (int i = 0; i < 3; i++) {
+            sb.append(ch[i]);
+        }
+        sb.append('-');
+        sb.append(ch[3]);
+        sb.append('-');
+
+        // 当第7位为0时，都是'XX-XXXXXX'格式，
+        // 当第7位为1~3时，都是'XXX-XXXXX'格式，
+        // 当第7位是5时，都是'XXXX-XXXX'格式
+        if (ch[4] == '0') {
+            // 0
+            for (int i = 4; i < 6; i++) {
+                sb.append(ch[i]);
+            }
+            sb.append('-');
+            for (int i = 6; i < 12; i++) {
+                sb.append(ch[i]);
+            }
+        } else if (ch[4] >= '1' && ch[4] <= '3') {
+            // 1 ~ 3
+            for (int i = 4; i < 7; i++) {
+                sb.append(ch[i]);
+            }
+            sb.append('-');
+            for (int i = 7; i < 12; i++) {
+                sb.append(ch[i]);
+            }
+        } else if (ch[4] == '5') {
+            // 5
+            for (int i = 4; i < 8; i++) {
+                sb.append(ch[i]);
+            }
+            sb.append('-');
+            for (int i = 8; i < 12; i++) {
+                sb.append(ch[i]);
+            }
+        } else {
+            for (int i = 4; i < 7; i++) {
+                sb.append(ch[i]);
+            }
+            sb.append('-');
+            for (int i = 7; i < 12; i++) {
+                sb.append(ch[i]);
+            }
+        }
+
+        // 最后也是固定的1位校验
+        sb.append('-');
+        sb.append(ch[12]);
+
+        return sb.toString();
     }
 
 

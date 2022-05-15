bd35cb3078c893a573a1f6fa0b50144c7cf498f2
xduTd <973958891@qq.com>
2022-05-15 17:51:18 +0800

    增加了必要的注释

--- src/main/java/xdu/backend/exception/UserNotExistsException.java
+++ src/main/java/xdu/backend/exception/UserNotExistsException.java
@@ -4,6 +4,7 @@ package xdu.backend.exception;
  * @author 邓乐丰
  */
 public class UserNotExistsException extends Exception {
+
     public UserNotExistsException(String userID) {
         super("The user of ID:" + userID + " doesn't exists, please register first.");
     }
